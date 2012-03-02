9bef4197eafe4d753d98245401d2dbe3d5763578
markelos <markeleas@gmail.com>
2012-03-02 22:42:34 +0200

    changed script due to new sig files

--- firefox-beta-bin/firefox.sh
+++ firefox-beta-bin/firefox.sh
@@ -14,8 +14,8 @@ mv SHA1SUMS SHA1SUMS-$pkgver
 if [ -f $sha ]; then
 
 # Defining variables
-newsha64=`grep -w "linux-x86_64/en-US/firefox-$pkgver.tar.bz2" $sha | awk {'print $1'}`
-newsha32=`grep -w "linux-i686/en-US/firefox-$pkgver.tar.bz2" $sha | awk {'print $1'}`
+newsha64=`grep -w "linux-x86_64/en-US/firefox-$pkgver.tar.bz2" $sha | awk 'NR==1{print $1}'`
+newsha32=`grep -w "linux-i686/en-US/firefox-$pkgver.tar.bz2" $sha | awk 'NR==1{print $1}'`
 
 oldsha64=`grep sha1sums $path | head -n1 | cut -c 12-51`
 oldsha32=`grep sha1sums $path | tail -n1 | cut -c 42-81`
@@ -53,7 +53,7 @@ echo "Uploading to AUR in 2 ..."
 sleep 1
 echo "Uploading to AUR in 1 ..."
 sleep 1
-burp firefox-beta-bin-$pkgver-1.src.tar.gz
+burp $(source PKGBUILD; echo ${pkgname}-${pkgver}-${pkgrel}.src.tar.gz) 
 echo
 echo "Upload complete."
 echo
@@ -77,6 +77,6 @@ read install
 rm $sha
 rm firefox-beta-bin-$pkgver-1.src.tar.gz
 rm firefox-beta-bin-$pkgver-1-i686.pkg.tar.xz
-
+rm firefox-$pkgver.tar.bz2
 
 
